9225015033cf4af44a40778e48061a707c3cb2cd
Devanshu Shrivastava <Devanshu Shrivastava@DESKTOP-57E8I4B>
2024-06-17 10:57:29 +0530

    my class is added

---
+++ AutomationFramework08/src/test/java/devanshu/Devanshuclass.java
@@ -0,0 +1,15 @@
+package devanshu;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Devanshuclass {
+		@Test
+		public void test()
+		{
+			Reporter.log("Devanshu class added",true);
+			Reporter.log("My class is added", true);
+		}
+	
+
+}
